76f342a1035212ef9d1f69e14622dd6010680cf3
Guillermo Bandrés Catalina <mail@gbandres.com>
2022-04-18 17:25:56 +0200

    deploy script

--- scripts/gh-pages-deploy.mjs
+++ scripts/gh-pages-deploy.mjs
@@ -13,7 +13,6 @@ import fs from "fs";
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("rmdir", ["/S", folderName]);
     await execa("git", ["checkout", "-f", "main"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
