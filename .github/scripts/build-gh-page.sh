1566e8d6803f6624d1304b72353097fda341dc6f
Guillermo Bandrés Catalina <mail@gbandres.com>
2022-04-21 12:52:18 +0200

    deploy action

---
+++ .github/scripts/build-gh-page.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git checkout --orphan gh-pages
+
+echo "Building started...");
+
+npm run build
+
+git --work-tree dist add --all
+git --work-tree dist commit -m "gh-pages"
+
+echo "Pushing to gh-pages..."
+
+git push origin HEAD:gh-pages --force
